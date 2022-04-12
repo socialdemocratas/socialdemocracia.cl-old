7879ea582b6fc66458ea731d1661e046deb26a0f
= <r.gnzlz.cstll@gmail.com>
2022-04-12 01:46:14 -0400

    experiment with cloudflare

--- src/routes/articulo/[id].js
+++ src/routes/articulo/[id].js
@@ -1,7 +1,7 @@
 
 export async function get({ cf, params, platform }) {
 
-    await platform.env.ARTICLES.put('test', 'hola');
+    await platform.env.ARTICLES.list('test');
 
     return {
         body: {
